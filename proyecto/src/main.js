ce59a4396c26fc39ad15ca1ee4ba103edee03c34
unknown <CIAC@CIAC2-021.academic.mixcoac.upmx.mx>
2023-05-19 11:32:28 -0600

    proyecto2

--- proyecto/src/main.js
+++ proyecto/src/main.js
@@ -21,12 +21,42 @@ import '@ionic/vue/css/flex-utils.css';
 import '@ionic/vue/css/display.css';
 
 /* Theme variables */
+
+
+//////////////////////////////////////////// firebase /////////////////////////////////////////////////////////////
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTJexWL-wh-cyQcVGu6q7q1JKPLeg-eN4",
+  authDomain: "practicasandroid-2c7b1.firebaseapp.com",
+  databaseURL: "https://practicasandroid-2c7b1-default-rtdb.firebaseio.com",
+  projectId: "practicasandroid-2c7b1",
+  storageBucket: "practicasandroid-2c7b1.appspot.com",
+  messagingSenderId: "103162547349",
+  appId: "1:103162547349:web:f087a6bb39092d82805f9c"
+};
+
+// Initialize Firebase
+const appfirebase = initializeApp(firebaseConfig);
+
+
+//////////////////////////////////////////// firebase /////////////////////////////////////////////////////////////
+
+
+
 import './theme/variables.css';
 
 const app = createApp(App)
   .use(IonicVue)
+  .use(appfirebase)
   .use(router);
   
 router.isReady().then(() => {
   app.mount('#app');
 });
+
+
